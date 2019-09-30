7bca99478af5fbe753f1dd0f70a426b6b3c377b7
Lasse <soxdrengen@gmail.com>
2019-09-30 09:58:18 +0200

    Tilføjet kommentarer

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    static final int antalKast = 1000;
 
     public static void main(String[] args) {
         System.out.println("Terningespil");
-        for (int i = 0; i < 100; i++) {
-            System.out.println("Terningen er kastet: " + Terning.cast());
-        }
+        /*
+        Vi opretter en ny ArrayList kaldet liste og sætter værdien fra Terning.flereKast(antalKast) til at være
+        lig med det andet ArrayList fra vores anden Terning klasse
+         */
+        ArrayList liste = Terning.flereKast(antalKast);
+
+        /*
+        For hver gang der biver fundet noget nyt i vores liste så printer vi en ny linje og en besked ud
+        Det vil sige at vi kaster vores terning 1000 gange og får 1000 tilfældige tal da metoderne kalder hinanden
+         */
+        liste.forEach((n) -> System.out.println("Terningen er kastet: " + n));
+        System.out.println("Terningen er kastet " + antalKast + " gange.");
     }
 }
--- src/Terning.java
+++ src/Terning.java
@@ -1,8 +1,33 @@
+import java.util.ArrayList;
+
 public class Terning {
 
-protected static int cast() {
+    /*
+    Her har vi to Arrays det ene er defineret til maks at indenholde 3 punkter
+    Mens vores ArrayList kan indenholde så mange punkter som vi ønsker.
+    Vi benytter kun ArrayList i dette eksempel
+     */
+    int alleKast[] = {1, 2, 3}; // 1 = plads 0, 2 = plads 1, 3 = plads 2
+    static ArrayList alleKastArrayList = new ArrayList();
+
+    //Vi gernere et tal mellem 1 og 6 ved hjælp af Math.Random()
+    protected static int cast() {
     int number = (int) (Math.random() * 6 + 1);
     return number;
 }
+
+    /*
+    Vi opretter en int metode hvor vi tager en int ind kaldet antal som vi manipulere inde i vores main metode
+    Denne metode er af typen ArrayList.
+    Vi benytter et for loop til at tjekke op fra main metoden hvor mange gange vi ønsker loopet skal køres igennem
+    For hver gang loopet køres igennem tilføjer vi det nye tal som bliver gerneret i Terning.cast() til vores ArrayListe
+    Når loopet er kørt igennem så retunere vi vores ArrayList
+     */
+    protected static ArrayList flereKast(int antal) {
+        for (int i = 0; i < antal; i++) {
+            alleKastArrayList.add(Terning.cast());
+        }
+            return alleKastArrayList;
+    }
 
 }
